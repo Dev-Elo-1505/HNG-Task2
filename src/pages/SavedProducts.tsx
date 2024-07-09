1f836a57a5c7462956f953dca89b94504c2e3364
Joy Addisi <elooghenejoy@gmail.com>
2024-07-10 00:20:42 +0100

    Build Info Component

--- src/pages/SavedProducts.tsx
+++ src/pages/SavedProducts.tsx
@@ -1,8 +1,22 @@
-import NavBar from "../components/NavBar"
-
+import NavBar from "../components/NavBar";
 
 export const SavedProducts = () => {
   return (
-    <><NavBar /></>
-  )
-}
+    <>
+      <NavBar />
+      <section>
+        <div id="info" className="px-5 py-6">
+          <div className="mb-4">
+            <h3 className="text-base font-medium">Buyer’s Information</h3>
+            <p className="text-customLink text-sm">Enter your details</p>
+          </div>
+          <form>
+            <input type="text" placeholder="Buyer's Name" className="border border-customLink p-4 rounded-lg w-full block focus:outline-none mb-10" required/>
+            <input type="number" placeholder="Phone Number" className="border border-customLink p-4 rounded-lg w-full block focus:outline-none mb-10" required/>
+            <input type="text" placeholder="Shipping Address" className="border border-customLink p-4 rounded-lg w-full block focus:outline-none mb-10" required/>
+          </form>
+        </div>
+      </section>
+    </>
+  );
+};
