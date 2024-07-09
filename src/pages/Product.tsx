7d35d0943e661d817b90b9476a4f62eb70a6e934
Joy Addisi <elooghenejoy@gmail.com>
2024-07-09 21:53:06 +0100

    Update Product Page

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -8,41 +8,41 @@ export const Product = () => {
   return (
     <>
       <NavBar />
-      <section className="py-6 px-5">
+      <section  className="py-6 px-5">
         <div>
           <h2 className="font-medium text-xl mb-6">Product</h2>
-          <div className="flex flex-col gap-6 md:flex-row">
+          <div className="flex flex-col gap-6 md:flex-row md:px-20 md:justify-between">
             <div className="flex justify-center items-center bg-customProductBgGrey rounded-[10px]">
               <img src={NivaLeather} alt="Niva Leather Shoe" />
             </div>
             <div>
               <div className="flex justify-between">
-                <div>
+                <div className="text-xl leading-10">
                   Nivea Leather Shoe <br />
-                  <span>₦31,000</span>
+                  <span className="text-2xl text-customGreen font-bold">₦31,000</span>
                 </div>
                 <div>
                   <img src={Star} alt="Top rated" />
                 </div>
               </div>
               <div className="flex justify-between">
-                <div>
-                  <h3>Color</h3>
+                <div className="leading-10">
+                  <h3 className="font-semibold">Color</h3>
                   <div className="flex gap-[0.625rem]">
                     <div className="rounded-full w-6 h-6 bg-[#000000]"></div>
                   </div>
                 </div>
-                <div>
-                  <h3>Quantity</h3>
+                <div className="leading-10">
+                  <h3 className="font-semibold">Quantity</h3>
                   <div className="flex gap-[0.625rem]">
-                    <div className="rounded-full w-6 h-6 bg-[#000000]">-</div>
-                    <div>1</div>
-                    <div className="rounded-full w-6 h-6 bg-[#160751]">+</div>
+                    <div className="rounded-full w-6 h-6 bg-[#000000] flex justify-center items-center">-</div>
+                    <div className="flex justify-center items-center">1</div>
+                    <div className="rounded-full w-6 h-6 bg-[#000000] flex justify-center items-center text-white">+</div>
                   </div>
                 </div>
               </div>
-              <div>
-                <p>Available sizes</p>
+              <div className="mb-2">
+                <p className="mb-4">Available sizes</p>
                 <div className="flex justify-between items-center">
                   <div className="py-[11px] px-2 rounded-lg bg-[#151515] text-[#FEFEFE]">
                     30 - 32
@@ -67,8 +67,10 @@ export const Product = () => {
               </div>
             </div>
           </div>
-          <div className="mb-3">Related Items</div>
-        <Showcase />
+          <div className="md:px-20">
+            <div className="mb-3 mt-6">Related Items</div>
+                    <Showcase />
+          </div>
         </div>
       </section>
     </>
