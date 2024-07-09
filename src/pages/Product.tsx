975f3143445b9d2a00e10d439430f7178a92be93
Joy Addisi <elooghenejoy@gmail.com>
2024-07-09 21:27:57 +0100

    Build Product Page

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,6 +1,8 @@
 import NavBar from "../components/NavBar";
 import NivaLeather from "../assets/img/niva-leather-shoe.svg";
 import Star from "../assets/icons/star.svg";
+import { Link } from "react-router-dom";
+import { Showcase } from "../components/Showcase";
 
 export const Product = () => {
   return (
@@ -38,12 +40,35 @@ export const Product = () => {
                     <div className="rounded-full w-6 h-6 bg-[#160751]">+</div>
                   </div>
                 </div>
-                <div>
-                  <p>Available sizes</p>
+              </div>
+              <div>
+                <p>Available sizes</p>
+                <div className="flex justify-between items-center">
+                  <div className="py-[11px] px-2 rounded-lg bg-[#151515] text-[#FEFEFE]">
+                    30 - 32
+                  </div>
+                  <div>33 - 35</div>
+                  <div>36 - 38</div>
+                  <div>39 - 41</div>
+                  <div>43 - 45</div>
                 </div>
+              </div>
+              <div className="flex justify-between">
+                <Link to="/cart">
+                  <button className="w-full py-5 px-6 bg-[#151515] rounded-lg text-[#fefefe] font-medium hover:opacity-90 active:scale-95 transition ease-in-out delay-150">
+                    Add to Cart
+                  </button>
+                </Link>
+                <Link to="/">
+                  <button className=" border border-black w-full py-5 px-6 bg-[#fefefe] rounded-lg text-[#151515] font-medium hover:opacity-90 active:scale-95 transition ease-in-out delay-150">
+                    Save For Later
+                  </button>
+                </Link>
               </div>
             </div>
           </div>
+          <div className="mb-3">Related Items</div>
+        <Showcase />
         </div>
       </section>
     </>
