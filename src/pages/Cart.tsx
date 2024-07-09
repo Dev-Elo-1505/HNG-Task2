8598094a5942ee9c284e8753479401c489ba8101
Joy Addisi <elooghenejoy@gmail.com>
2024-07-09 16:39:19 +0100

    Build Cart Page

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,8 +1,84 @@
-import NavBar from "../components/NavBar"
-
+import NavBar from "../components/NavBar";
+import NivaShoe from "../assets/img/niva-leather-shoe.svg";
+import VictoriShoe from "../assets/img/victori-one-slide.svg";
+import RedLuminousShoe from "../assets/img/red-luminous-shoe.svg";
 
 export const Cart = () => {
   return (
-    <><NavBar /></>
-  )
-}
+    <>
+      <NavBar />
+      <section className="px-5 py-6 flex flex-col md:px-[7.5rem] md:py-20">
+        <div className="flex-grow min-h-screen">
+          <div className="flex justify-between items-center text-xl mb-[10px]">
+            <p className="font-medium md:text-xl">My Cart</p>
+            <p className="text-customGreen">Edit</p>
+          </div>
+          <div>
+            <div className="p-2 flex items-center justify-between mb-4 md:shadow-custom-medium md:rounded-lg md:p-4 md:mb-6">
+              <div className="flex gap-4">
+                <div className="bg-customProductBgGrey rounded-lg flex justify-center items-center w-[8.125rem]">
+                  <img src={NivaShoe} alt="Niva Leather Shoe" />
+                </div>
+                <div>
+                  <p className="font-semibold text-sm text-customLink mb-4">
+                    Niva Leather (Black) <br />
+                    Color: Black <br />
+                    Qty: 1
+                  </p>
+                  <p className="font-bold text-base text-customGreen">₦31,000</p>
+                </div>
+              </div>
+              <div className="p-1 border border-black rounded-full w-6 h-6 bg-[#FEFEFE] flex items-center justify-center font-extrabold">
+                -
+              </div>
+            </div>
+            <div className="p-2 flex gap-4 items-center justify-between mb-4 md:shadow-custom-medium md:rounded-lg md:p-4 md:mb-6">
+              <div className="flex gap-4">
+                <div className="bg-[#E1E3E2] rounded-lg flex justify-center items-center w-[8.125rem]">
+                  <img src={VictoriShoe} alt="Victori One Shoe" />
+                </div>
+                <div>
+                  <p className="font-semibold text-sm text-customLink mb-4">
+                  Nike Victori One Slide <br />
+                    Color: Black <br />
+                    Qty: 1
+                  </p>
+                  <p className="font-bold text-base text-customGreen">₦13,000</p>
+                </div>
+              </div>
+              <div className="p-1 border border-black rounded-full w-6 h-6 bg-[#FEFEFE] flex items-center justify-center font-extrabold">
+                -
+              </div>
+            </div>
+            <div className="p-2 flex gap-4 items-center justify-between mb-4 md:shadow-custom-medium md:rounded-lg md:p-4 md:mb-6">
+              <div className="flex gap-4">
+                <div className="rounded-lg flex justify-center items-center w-[8.125rem]">
+                  <img src={RedLuminousShoe} alt="Red Luminous Shoe" />
+                </div>
+                <div>
+                  <p className="font-semibold text-sm text-customLink mb-4">
+                    Kids Led Luminous Shoe <br />
+                    Color: Red <br />
+                    Qty: 1
+                  </p>
+                  <p className="font-bold text-base text-customGreen">₦23,000</p>
+                </div>
+              </div>
+              <div className="p-1 border border-black rounded-full w-6 h-6 bg-[#FEFEFE] flex items-center justify-center font-extrabold">
+                -
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="p-5">
+      <div className="flex justify-between items-center text-xl mb-10">
+          <p className="font-medium text-base">Sub-Total</p>
+          <p className="text-customGreen font-bold text-xl">₦67,000</p>
+        </div>
+        <button className="w-full py-5 px-6 bg-[#151515] rounded-lg text-[#fefefe] font-medium">Checkout</button>
+      </div>
+      </section>
+      
+    </>
+  );
+};
--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,5 +1,6 @@
 import NavBar from "../components/NavBar";
 import NivaLeather from "../assets/img/niva-leather-shoe.svg";
+import Star from "../assets/icons/star.svg";
 
 export const Product = () => {
   return (
@@ -8,11 +9,51 @@ export const Product = () => {
       <section className="py-6 px-5">
         <div>
           <h2 className="font-medium text-xl mb-6">Product</h2>
-          <div className="flex flex-col gap-6">
-            <div className="bg-customProductBgGrey rounded-[10px]">
+          <div className="flex flex-col gap-6 md:flex-row">
+            <div className="flex justify-center items-center bg-customProductBgGrey rounded-[10px]">
               <img src={NivaLeather} alt="Niva Leather Shoe" />
             </div>
-            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae maxime voluptate hic aspernatur maiores libero soluta fuga. Dolorum, obcaecati cumque?</div>
+            <div>
+              <div className="flex justify-between">
+                <div>
+                  Nivea Leather Shoe <br />
+                  <span>₦31,000</span>
+                </div>
+                <div>
+                  <img src={Star} alt="Top rated" />
+                </div>
+              </div>
+              <div>
+                <h3>Product description</h3>
+                <p>
+                  Niva isn't just a shoe brand, it's a legacy. Since 1982, these
+                  kicks have been a staple on the court,the streets, and ever...
+                </p>
+                <p className="text-right">See More</p>
+              </div>
+              <div className="flex justify-between">
+                <div>
+                  <h3>Color</h3>
+                  <div className="flex gap-[0.625rem]">
+                    <div
+                      className="rounded-full w-6 h-6 bg-[#000000]"
+                    ></div>
+                    <div className="border border-black rounded-full w-6 h-6 bg-[#FEFEFE]"></div>
+                    <div className="rounded-full w-6 h-6 bg-[#160751]"></div>
+                  </div>
+                </div>
+                <div>
+                <h3>Quantity</h3>
+                  <div className="flex gap-[0.625rem]">
+                    <div
+                      className="rounded-full w-6 h-6 bg-[#000000]"
+                    >-</div>
+                    <div className="border border-black rounded-full w-6 h-6 bg-[#FEFEFE]"></div>
+                    <div className="rounded-full w-6 h-6 bg-[#160751]">+</div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
