770da682111ca161788e7c64210f0aae669a4c8f
Joy Addisi <elooghenejoy@gmail.com>
2024-07-10 01:03:56 +0100

    Update Buyers page

--- src/pages/SavedProducts.tsx
+++ src/pages/SavedProducts.tsx
@@ -5,7 +5,7 @@ export const SavedProducts = () => {
   return (
     <>
       <NavBar />
-      <section className="px-5 py-6">
+      <section className="px-5 py-6 md:py-20 md:px-[120px]">
         <div id="info">
           <div className="mb-4">
             <h3 className="text-base font-medium">Buyer’s Information</h3>
@@ -39,6 +39,16 @@ export const SavedProducts = () => {
               Choose your preferred delivery company
             </p>
           </div>
+          <form>
+            <div className="flex justify-between mb-10">
+              <div className="flex gap-4"><input type="radio" /><p className="text-base font-medium text-black">GIG Logistics <br /><span className="text-sm font-normal text-customLink">2 days after order</span></p></div>
+              <p className="text-customGreen font-semibold">₦3000</p>
+            </div>
+            <div className="flex justify-between mb-10">
+              <div className="flex gap-4"><input type="radio" /><p className="text-base font-medium text-black">Swift Logistics <br /><span className="text-sm font-normal text-customLink">1 day after order</span></p></div>
+              <p className="text-customGreen font-semibold">₦5000</p>
+            </div>
+          </form>
         </div>
         <div className="px-5 py-6">
           <div className="mb-4">
@@ -74,9 +84,18 @@ export const SavedProducts = () => {
             </div>
           </form>
           <div id="summary">
-            <div className="flex justify-between mb-4"><p>Sub-Total</p><p className="text-customGreen">₦67,000</p></div>
-            <div className="flex justify-between mb-4"><p>Delivery Fee</p><p className="text-customGreen">₦3000</p></div>
-            <div className="flex justify-between mb-4"><p>Total</p><p className="text-customGreen">₦72,000</p></div>
+            <div className="flex justify-between mb-4">
+              <p>Sub-Total</p>
+              <p className="text-customGreen">₦67,000</p>
+            </div>
+            <div className="flex justify-between mb-4">
+              <p>Delivery Fee</p>
+              <p className="text-customGreen">₦3000</p>
+            </div>
+            <div className="flex justify-between mb-4">
+              <p>Total</p>
+              <p className="text-customGreen">₦72,000</p>
+            </div>
           </div>
         </div>
         <Link to="/checkout">
